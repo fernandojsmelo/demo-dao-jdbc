d6c80d17f251abc59b7020aae80ea1c2a7da489f
fernandojsmelo <fernandojsmelo@gmail.com>
2023-10-26 09:35:17 -0300

    Department entity class

---
+++ src/application/Programa.java
@@ -0,0 +1,13 @@
+package application;
+
+import model_entities.Department;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		Department obj = new Department(1, "Books");
+		System.out.println(obj);
+
+	}
+
+}
